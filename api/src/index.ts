815dd7b86a6b23737cf40d637e2ebfb17942dd58
ketdevbanban <125855289+ketdevbanban@users.noreply.github.com>
2023-04-19 13:07:34 +0700

    add constriller,validation file

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,6 @@
 import express,{Request,Response} from "express";
 import cors from "cors";
+import { routes } from "./routes";
 
 const app = express();
 
@@ -8,7 +9,5 @@ app.use(cors({
     origin:["http://localhost:3000"]
 }))
 
-app.get("/",(req:Request, res:Response) => {
-    res.send("ket dev ban ban")
-})
+routes(app);
 app.listen(8000,() => {console.log("listen to port 8000")})
