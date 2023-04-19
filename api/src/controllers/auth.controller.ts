e9c1a83d3a44b5fed1463b1a99f96513094e22a9
ketdevbanban <125855289+ketdevbanban@users.noreply.github.com>
2023-04-19 14:29:43 +0700

    add connection to database

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -1,4 +1,17 @@
 import { Request,Response } from "express";
+import { RegissterValidation } from "../validation/register.validation";
 export const Register = (req: Request, res: Response) =>{
-    res.send(req.body)
+    const body = req.body;
+    const {error}=RegissterValidation.validate(body);
+    if(error){
+        return res.status(400).send(error.details)
+    }
+
+    if(body.password !==body.password_confirm){
+        return res.status(400).send({
+            message:"Password's do not match"
+        })
+
+    }
+    res.send(body)
 }
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,19 @@
 import express,{Request,Response} from "express";
 import cors from "cors";
 import { routes } from "./routes";
+import {createConnection} from "typeorm";
 
-const app = express();
+createConnection().then(connection => {
+    const app = express();
+    app.use(express.json());
+    app.use(cors({
+        credentials: true,
+        origin: ["http://localhost:3000"]
+    }));
 
-app.use(express.json());
-app.use(cors({
-    origin:["http://localhost:3000"]
-}))
+    routes(app);
+    app.listen(8000, () => {
+        console.log('listening to port 8000')
+    });
+});
 
-routes(app);
-app.listen(8000,() => {console.log("listen to port 8000")})
