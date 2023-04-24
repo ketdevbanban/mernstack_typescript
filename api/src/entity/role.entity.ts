ea25aa91279df770de1923ea0c64489716beedfd
ketdevbanban <125855289+ketdevbanban@users.noreply.github.com>
2023-04-24 13:42:12 +0700

    add Perimission Entity and role seed and add roles:seed in package.json

--- api/src/entity/role.entity.ts
+++ api/src/entity/role.entity.ts
@@ -1,8 +1,19 @@
-import { Entity,Column,PrimaryGeneratedColumn} from "typeorm";
+import { Permission } from './permission.entity';
+import { Entity,Column,PrimaryGeneratedColumn, ManyToMany, JoinTable} from "typeorm";
 @Entity()
 export class Role {
   @PrimaryGeneratedColumn()
   id: number;
  @Column()
   name: string;
+
+  @ManyToMany(() => Permission)
+  @JoinTable({
+      name: 'role_permissions',
+      joinColumn: {name: 'role_id', referencedColumnName: 'id'},
+      inverseJoinColumn: {name: 'permission_id', referencedColumnName: 'id'}
+  })
+
+permissions: Permission[];
+
 }
